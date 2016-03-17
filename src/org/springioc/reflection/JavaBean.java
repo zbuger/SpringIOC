6cc70c3a54db8370ddcc139677b75a55dcd08b23
zbuger <sdzhaoguoqin@qq.com>
2016-03-17 10:16:49 +0800

    spring ioc

---
+++ src/org/springioc/reflection/JavaBean.java
@@ -0,0 +1,23 @@
+package org.springioc.reflection;
+
+public class JavaBean {
+	private String userName;
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
